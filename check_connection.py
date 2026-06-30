98f45559ba7943ed68774890c2ce6761c49975d3
Alex <appwork1020@gmail.com>
2026-06-30 10:46:56 +0200

    Ingestion strategy outline

---
+++ check_connection.py
@@ -0,0 +1,10 @@
+# 1. Connect to azure account
+
+# 2. Try to read into polars and pandas 
+
+# 3. If all is good then print "Ready for EDA!" 
+
+# 4. Close connection (I hope its done automatically)
+
+if __name__ == "__main__":
+    main()
---
+++ ingest.py
@@ -0,0 +1,13 @@
+# 1. Connect to azure account
+
+# 2. Check whether the dataset is loaded
+# 2.1 If loaded --> exit
+
+# 3. Fetch the dataset from kaggle
+
+# 4. Load the dataset into azure cloud
+
+# 5. Close connection (I hope its done automatically)
+
+if __name__ == "__main__":
+    main()
